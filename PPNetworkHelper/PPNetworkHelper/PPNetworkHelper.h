52f553ae5bc2f32889cd953abbc95fc2f75ec6d7
JiangLong <983220205@qq.com>
2023-04-21 10:36:26 +0800

    上传图片方法新增Header参数

--- PPNetworkHelper/PPNetworkHelper/PPNetworkHelper.h
+++ PPNetworkHelper/PPNetworkHelper/PPNetworkHelper.h
@@ -234,6 +234,34 @@ typedef void(^PPNetworkStatus)(PPNetworkStatusType status);
                                            failure:(PPHttpRequestFailed)failure;
 
 /**
+ *  上传单/多张图片
+ *
+ *  @param URL        请求地址
+ *  @param parameters 请求参数
+ *  @param name       图片对应服务器上的字段
+ *  @param images     图片数组
+ *  @param fileNames  图片文件名数组, 可以为nil, 数组内的文件名默认为当前日期时间"yyyyMMddHHmmss"
+ *  @param imageScale 图片文件压缩比 范围 (0.f ~ 1.f)
+ *  @param imageType  图片文件的类型,例:png、jpg(默认类型)....
+ *  @param progress   上传进度信息
+ *  @param success    请求成功的回调
+ *  @param failure    请求失败的回调
+ *
+ *  @return 返回的对象可取消请求,调用cancel方法
+ */
++ (__kindof NSURLSessionTask *)uploadImagesWithURL:(NSString *)URL
+                                        parameters:(id)parameters
+                                              name:(NSString *)name
+                                            images:(NSArray<UIImage *> *)images
+                                         fileNames:(NSArray<NSString *> *)fileNames
+                                        imageScale:(CGFloat)imageScale
+                                         imageType:(NSString *)imageType
+                                           headers:(nullable NSDictionary <NSString *, NSString *> *)headers
+                                          progress:(PPHttpProgress)progress
+                                           success:(PPHttpRequestSuccess)success
+                                           failure:(PPHttpRequestFailed)failure;
+
+/**
  *  下载文件
  *
  *  @param URL      请求地址
